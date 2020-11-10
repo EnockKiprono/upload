b9e763eaaebed69d6e59240523f7b20353f47d10
EnockKiprono <enockkipronoh2@gmail.com>
2020-11-10 16:19:07 +0300

    php script to upload images into the database in a webpage

---
+++ upload.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>recent posts</title>
+</head>
+<body>
+	<?php
+
+   if(isset($_FILES['image'])){
+
+      $errors= array();
+
+      $file_name = $_FILES['image']['name'];
+
+      $file_size = $_FILES['image']['size'];
+
+      $file_tmp = $_FILES['image']['tmp_name'];
+
+      $file_type = $_FILES['image']['type'];
+
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+     
+
+      $expensions= array("jpeg","jpg","png");
+
+     
+
+      if(in_array($file_ext,$expensions)=== false){
+
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+
+      }
+
+     
+
+      if($file_size > 2097152) {
+
+         $errors[]='File size must be excately 2 MB';
+
+      }
+
+     
+
+      if(empty($errors)==true) {
+
+         move_uploaded_file($file_tmp,"images/".$file_name);
+
+         echo "Success";
+
+      }else{
+
+         print_r($errors);
+
+      }
+
+   }
+
+?>
+
+<html>
+
+   <body>
+
+     
+
+      <form action = "image.html" method = "POST" enctype = "multipart/form-data">
+
+         <input type = "file" name = "image" />
+
+         <input type = "submit"/>
+
+                                    
+
+         <ul>
+
+            <li>Sent file: <?php echo $_FILES['image']['name'];  ?>
+
+            <li>File size: <?php echo $_FILES['image']['size'];  ?>
+
+            <li>File type: <?php echo $_FILES['image']['type'] ?>
+
+         </ul>
+
+                                    
+
+      </form><br>
+      <div id="home">
+      <a href="index.html"><em style="color: blue">back to homepage</em></a>
+   </div>
+
+     
+
+   </body>
+
+</html>
+</body>
+</html>
